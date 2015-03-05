3e72ad89e6cc96d4a6ef4df855b27f6b42b8e6c5
MLee21 <minnie.lee89@gmail.com>
2015-03-05 14:47:34 -0700

    building encryptor class.

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -1,27 +1,16 @@
-require_relative 'keys'
-require_relative 'off_set_calculator'
-require_relative 'character_map'
+require 'message.txt'
 
 class Encryptor
 
-  def initialize
-    @off_set = OffSetCalculator.new
-    @key = Keys.new
+    def initialize(filename)
+      File.read(filename)
+    end
+
+    def file_reader(text)
+    
+    
+    end
   end
-
-
-  def add_rot_a_to_off_set
-    off_set_a = @key.rot_a 
-  end
-
-
-
-
-
-
-
-
-
 
 
 end
