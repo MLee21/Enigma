96313f31b330d5cbd2d1386373b1cf05861b26e9
MLee21 <minnie.lee89@gmail.com>
2015-03-07 21:05:30 -0700

    made sure to update tests to correspond with refactored method.

--- test/keys_test.rb
+++ test/keys_test.rb
@@ -24,37 +24,40 @@ attr_reader :key
 
   def test_rotation_A_will_return_first_index_of_array
     key.stub :key_generate, [4,6,3,8,9] do
-      assert_equal [46,63,38,89], key.all_rotations
+      assert_equal 46, key.all_rotations[0]
     end
   end
 
   def test_rotation_A_will_return_zero_and_digit
     key.stub :key_generate, [0,1,2,3,4] do
-      assert_equal [01,12,23,34], key.all_rotations
+      assert_equal 01, key.all_rotations[0]
     end
   end
 
   def test_rotation_A_will_return_zero_and_zero
     key.stub :key_generate, [0,0,0,0,0] do
-      assert_equal [00,00,00,00], key.all_rotations
+      assert_equal 00, key.all_rotations[0]
     end
   end
 
-  def test_second_and_third_digits_equals_rotation_B
+  def test_second_and_third_digits_equals_rotation_b
     key.stub :key_generate, [6,5,7,9,8] do
-      assert_equal [65,57,79,98], key.all_rotations
+      assert_equal 57, key.all_rotations[1]
     end 
-  end
-
-  def test_third_and_fourth_digits_equals_rotation_C
-    key.stub :key_generate, [3,6,4,2,4] do
-      assert_equal [36,64,42,24], key.all_rotations
+    key.stub :key_generate, [6,5,7,9,8] do
+      refute_equal 65, key.all_rotations[1]
     end
   end
 
-  def test_fourth_and_fifth_digits_equals_rotation_D
+  def test_third_and_fourth_digits_equals_rotation_c
+    key.stub :key_generate, [3,6,4,2,4] do
+      assert_equal 42, key.all_rotations[2]
+    end
+  end
+
+  def test_fourth_and_fifth_digits_equals_rotation_d
     key.stub :key_generate, [5,1,2,3,0] do
-      assert_equal [51,12,23,30], key.all_rotations
+      assert_equal 30, key.all_rotations[3]
     end
   end
 
